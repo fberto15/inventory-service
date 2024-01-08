0e7445975311c5cd49b6e0a8b2218011db4d0aef
Francisco Bertomeu <fberto15@gmail.com>
2024-01-07 21:43:20 -0300

    eureka

--- src/main/java/com/curso/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/curso/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.curso.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.curso.inventoryservice.model.Inventory;
 import com.curso.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
